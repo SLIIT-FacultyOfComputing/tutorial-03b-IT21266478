b193dfa73a62cc64c7112c10aab00c5ced476665
IT21266478 <99186783+IT21266478@users.noreply.github.com>
2022-02-24 18:53:35 +0000

    1,2,3 done 4 not completed

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
+
 using namespace std;
-int main() {
- float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
- char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
- for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
- }
+
+int main()
+{
+	float marks[]={78.40,90.60,45.90,72.20,54.40};
+	char names[][20]={"Ajith","Wimal","Kanthi","Suranji","Kushmitha"};
+	
+	cout << setw(10)<<setprecision(5)<< "No"
+	     << setw(20)<<setprecision(15)<< "Name" 
+		 << setw(10)<<setprecision(10)<<"Marks" 
+		 <<endl;
+	
+	for(int r=0;r<5;r++)
+	{
+		cout << setw(10)<<setprecision(5) << r+1
+		     << setw(20)<<setprecision(15) << names[r]
+		     << setw(10)<<setprecision(4)<< marks[r]<<endl;
+	}
 }
